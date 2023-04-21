6de80d85dc8c2f6c82578009e4febb0fd7eadcc0
fauzanfebrian <fauzanfebrian1725@gmail.com>
2023-04-21 15:59:50 +0700

    seo optimization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { AxiosError } from 'axios'
+import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
@@ -67,33 +68,75 @@ export default function Home() {
     const { user, artists, tracks, genres } = data
 
     return (
-        <main className="bg-img min-h-screen p-4 py-8 md:p-8 overflow-hidden">
-            {user && (
-                <section className="flex items-center">
-                    <Image
-                        src={user.images?.[0].url}
-                        alt="User photo profile"
-                        width={100}
-                        height={100}
-                        className="rounded-full mr-3 shadow-black shadow-sm"
-                    />
-                    <h1 className="text-xl md:text-3xl text-white">
-                        {user.display_name}
-                        {"'"}s{' '}
-                        <Link
-                            href={spotifyLink}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="font-gotham text-green-600"
-                        >
-                            Spotify
-                        </Link>
-                    </h1>
-                </section>
-            )}
-            {!!artists?.length && <Artists artists={artists} />}
-            {!!tracks?.length && <Tracks tracks={tracks} />}
-            {!!genres?.length && <Genres genres={genres} />}
-        </main>
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>
+                    {user.display_name}
+                    {"'"}s Top Tracks, Artists, and Genres on Spotify
+                </title>
+                <meta
+                    name="description"
+                    content={`Explore the top tracks, artists, and genres from ${user.display_name}'s personal Spotify account.`}
+                />
+                <meta
+                    name="keywords"
+                    content="Spotify, top tracks, top artists, top genres, music, playlist, discover"
+                />
+                <meta name="author" content={user.display_name} />
+                <meta name="robots" content="index, follow" />
+                <meta
+                    property="og:title"
+                    content={`${user.display_name}'s Top Tracks, Artists, and Genres on Spotify`}
+                />
+                <meta
+                    property="og:description"
+                    content={`Explore the top tracks, artists, and genres from ${user.display_name}'s personal Spotify account.`}
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://spotify.fauzanfebrian.my.id" />
+                <meta property="og:image" content={user.images?.[0].url} />
+                <meta property="og:image:width" content={(user.images?.[0]?.width || 500).toString()} />
+                <meta property="og:image:height" content={(user.images?.[0]?.height || 500).toString()} />
+                <meta
+                    name="twitter:title"
+                    content={`${user.display_name}'s Top Tracks, Artists, and Genres on Spotify`}
+                />
+                <meta
+                    name="twitter:description"
+                    content={`Explore the top tracks, artists, and genres from ${user.display_name}'s personal Spotify account.`}
+                />
+                <meta name="twitter:image" content={user.images?.[0].url} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <main className="bg-img min-h-screen p-4 py-8 md:p-8 overflow-hidden">
+                {user && (
+                    <section className="flex items-center">
+                        <Image
+                            src={user.images?.[0].url}
+                            alt="User photo profile"
+                            width={100}
+                            height={100}
+                            className="rounded-full mr-3 shadow-black shadow-sm"
+                        />
+                        <h1 className="text-xl md:text-3xl text-white">
+                            {user.display_name}
+                            {"'"}s{' '}
+                            <Link
+                                href={spotifyLink}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="font-gotham text-green-600"
+                            >
+                                Spotify
+                            </Link>
+                        </h1>
+                    </section>
+                )}
+                {!!artists?.length && <Artists artists={artists} />}
+                {!!tracks?.length && <Tracks tracks={tracks} />}
+                {!!genres?.length && <Genres genres={genres} />}
+            </main>
+        </>
     )
 }
