51f6346db0a3eaffcc4273721a2464db33ee04dd
fauzanfebrian <fauzanfebrian1725@gmail.com>
2023-11-11 13:31:01 +0700

    add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
+import Hotjar from '@hotjar/browser'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { RefObject, useEffect, useState } from 'react'
 import { AppContext } from 'src/context'
 import 'src/styles/globals.css'
-import Hotjar from '@hotjar/browser'
 
 export default function App({ Component, pageProps }: AppProps) {
     const [audioPlayer, setAudioPlayer] = useState<RefObject<HTMLAudioElement>>()
@@ -28,8 +29,18 @@ export default function App({ Component, pageProps }: AppProps) {
     }
 
     return (
-        <AppContext.Provider value={{ playAudio, pauseAudio }}>
-            <Component {...pageProps} />
-        </AppContext.Provider>
+        <>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-XPKPDC3WSB" />
+            <Script id="google-analytics">
+                {`window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                gtag('config', 'G-XPKPDC3WSB');`}
+            </Script>
+            <AppContext.Provider value={{ playAudio, pauseAudio }}>
+                <Component {...pageProps} />
+            </AppContext.Provider>
+        </>
     )
 }
