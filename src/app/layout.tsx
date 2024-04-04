1f63eed69d5701c0d7fb8e7205a15561c084284a
fauzanfebrian <fauzanfebrian1725@gmail.com>
2024-04-04 12:39:10 +0700

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import { GOOGLE_TAG_ID } from '@/config'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const poppims = Poppins({ weight: '400', subsets: ['latin'] })
@@ -21,6 +23,20 @@ export default function RootLayout({
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
             <body className={poppims.className}>{children}</body>
+
+            {!!GOOGLE_TAG_ID && (
+                <>
+                    <Script src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`} />
+                    <Script id="google-analytics">
+                        {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GOOGLE_TAG_ID}');
+                        `}
+                    </Script>
+                </>
+            )}
         </html>
     )
 }
